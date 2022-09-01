c4e0332c7aad538812b035daeb8185833133d147
Christoph Gohlke <cgohlke@cgohlke.com>
2022-09-01 10:15:31 -0700

    Update netpbmfile/netpbmfile.py

--- netpbmfile/netpbmfile.py
+++ netpbmfile/netpbmfile.py
@@ -36,71 +36,87 @@ format as specified at http://netpbm.sourceforge.net/doc/.
 
 The following Netpbm and Portable FloatMap formats are supported:
 
-* PBM (bi-level)
-* PGM (grayscale)
-* PPM (color)
-* PAM (arbitrary)
-* XV thumbnail (RGB332, read-only)
-* Pf (float32 grayscale, read-only)
-* PF (float32 RGB, read-only)
-* PF4 (float32 RGBA, read-only)
-* PGX (signed grayscale, read-only)
+- PBM (bi-level)
+- PGM (grayscale)
+- PPM (color)
+- PAM (arbitrary)
+- XV thumbnail (RGB332, read-only)
+- Pf (float32 grayscale, read-only)
+- PF (float32 RGB, read-only)
+- PF4 (float32 RGBA, read-only)
+- PGX (signed grayscale, read-only)
 
 No gamma correction is performed. Only one image per file is supported.
 
 The PGX format is specified in ITU-T Rec. T.803.
 
-:Author:
-  `Christoph Gohlke <https://www.lfd.uci.edu/~gohlke/>`_
-
-:Organization:
-  Laboratory for Fluorescence Dynamics, University of California, Irvine
-
+:Author: `Christoph Gohlke <https://www.cgohlke.com>`_
 :License: BSD 3-Clause
-
-:Version: 2022.2.2
+:Version: 2022.9.1
 
 Requirements
 ------------
+
 This release has been tested with the following requirements and dependencies
 (other versions may work):
 
-* `CPython 3.8.10, 3.9.10, 3.10.2 64-bit <https://www.python.org>`_
-* `Numpy 1.21.5 <https://pypi.org/project/numpy/>`_
-* `Matplotlib 3.4.3 <https://pypi.org/project/matplotlib/>`_  (optional)
+- `CPython 3.8.10, 3.9.13, 3.10.6, 3.11.0rc1 <https://www.python.org>`_
+- `NumPy 1.22.4 <https://pypi.org/project/numpy/>`_
+- `Matplotlib 3.5.3 <https://pypi.org/project/matplotlib/>`_  (optional)
 
 Revisions
 ---------
+
+2022.9.1
+
+- Update metadata.
+
 2022.2.2
-    Add type hints.
-    Support reading PF4 RGBA FloatMaps.
-    Drop support for Python 3.7 and numpy < 1.19 (NEP29).
+
+- Add type hints.
+- Support reading PF4 RGBA FloatMaps.
+- Drop support for Python 3.7 and numpy < 1.19 (NEP29).
+
 2021.6.6
-    Fix unclosed file warnings.
-    Support reading PGX JPEG2000 reference images.
+
+- Fix unclosed file warnings.
+- Support reading PGX JPEG2000 reference images.
+
 2020.10.18
-    Disallow comments after last value in PNM headers.
+
+- Disallow comments after last value in PNM headers.
+
 2020.9.18
-    Remove support for Python 3.6 (NEP 29).
-    Support os.PathLike file names.
+
+- Remove support for Python 3.6 (NEP 29).
+- Support os.PathLike file names.
+
 2020.1.1
-    Fix reading tightly packed P1 format and ASCII data with inline comments.
-    Remove support for Python 2.7 and 3.5.
-    Update copyright.
+
+- Fix reading tightly packed P1 format and ASCII data with inline comments.
+- Remove support for Python 2.7 and 3.5.
+- Update copyright.
+
 2018.10.18
-    Move netpbmfile.py into netpbmfile package.
+
+- Move netpbmfile.py into netpbmfile package.
+
 2018.02.18
-    Support reading Portable FloatMaps.
-    Style fixes.
+
+- Support reading Portable FloatMaps.
+- Style fixes.
+
 2016.02.24
-    Use 'fromdata' classmethod to initialize from data.
-    Support 'with' statement.
-    Scale RGB images to maxval for display.
-    Make keyword arguments explicit.
-    Support numpy 1.10.
+
+- Use fromdata classmethod to initialize from data.
+- Support with statement.
+- Scale RGB images to maxval for display.
+- Make keyword arguments explicit.
+- Support numpy 1.10.
 
 Examples
 --------
+
 Save a numpy array to a Netpbm file in grayscale format:
 
 >>> data = numpy.array([[0, 1], [65534, 65535]], dtype='uint16')
@@ -126,14 +142,15 @@ dtype('>u2')
 65535
 b'P5'
 
-To view the image stored in a Netpbm file from a command line, run
-``python -m netpbmfile _tmp.pgm``.
+View the image stored in a Netpbm file from a command line::
+
+    ``python -m netpbmfile _tmp.pgm``.
 
 """
 
 from __future__ import annotations
 
-__version__ = '2022.2.2'
+__version__ = '2022.9.1'
 
 __all__ = ['imread', 'imwrite', 'imsave', 'NetpbmFile']
 
@@ -142,18 +159,27 @@ import os
 import re
 import math
 import warnings
-from typing import BinaryIO, Literal, Union
 
 import numpy
 
 
-ArrayLike = Union[numpy.ndarray, list, tuple]
-ByteOrder = Union[Literal['>'], Literal['<']]
-PathLike = Union[str, os.PathLike]
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import BinaryIO, Literal, Union
+
+    try:
+        from numpy.typing import ArrayLike
+    except ImportError:
+        # numpy < 1.20
+        from numpy import ndarray as ArrayLike
+
+    ByteOrder = Union[Literal['>'], Literal['<']]
+    PathLike = Union[str, os.PathLike]
 
 
 def imread(
-    filename: PathLike, byteorder: ByteOrder | None = None
+    filename: PathLike, /, *, byteorder: ByteOrder | None = None
 ) -> numpy.ndarray:
     """Return image data from Netpbm file as numpy array."""
     with NetpbmFile(filename) as netpbm:
@@ -162,7 +188,12 @@ def imread(
 
 
 def imwrite(
-    filename: PathLike, data, maxval: int | None = None, pam: bool = False
+    filename: PathLike,
+    data,
+    maxval: int | None = None,
+    /,
+    *,
+    pam: bool = False,
 ) -> None:
     """Write image data to Netpbm file."""
     NetpbmFile.fromdata(data, maxval=maxval).write(filename, pam=pam)
@@ -204,7 +235,7 @@ class NetpbmFile:
         b'PG': b'PGX',
     }
 
-    def __init__(self, filename: PathLike | BinaryIO | None) -> None:
+    def __init__(self, filename: PathLike | BinaryIO | None, /) -> None:
         """Initialize instance from filename or open file."""
         self.header = b''
         self.width = 0
@@ -267,7 +298,9 @@ class NetpbmFile:
             self.axes = 'YX'
 
     @classmethod
-    def fromdata(cls, data: ArrayLike, maxval=None) -> NetpbmFile:
+    def fromdata(
+        cls, data: ArrayLike, /, *, maxval: int | None = None
+    ) -> NetpbmFile:
         """Initialize instance from numpy array."""
         data = numpy.array(data, ndmin=2, copy=True)
         if data.dtype.kind not in 'uib':
@@ -308,6 +341,7 @@ class NetpbmFile:
 
     def asarray(
         self,
+        *,
         copy: bool = True,
         cache: bool = False,
         byteorder: ByteOrder | None = None,
@@ -323,7 +357,9 @@ class NetpbmFile:
                 return data
         return numpy.copy(data) if copy else data
 
-    def write(self, filename: PathLike | BinaryIO, pam: bool = False) -> None:
+    def write(
+        self, filename: PathLike | BinaryIO, /, *, pam: bool = False
+    ) -> None:
         """Write instance to file."""
         if isinstance(filename, (str, os.PathLike)):
             with open(filename, 'wb') as fh:
@@ -338,7 +374,7 @@ class NetpbmFile:
             self._fh.close()
             self._fh = None
 
-    def _read_pam_header(self, data: bytes) -> None:
+    def _read_pam_header(self, data: bytes, /) -> None:
         """Read PAM header and initialize instance."""
         match = re.search(
             br'(^P7[\n\r]+(?:(?:[\n\r]+)|(?:#.*)|'
@@ -357,7 +393,7 @@ class NetpbmFile:
         matches = re.findall(br'(TUPLTYPE\s+\w+)', self.header)
         self.tupltypes = [s.split(None, 1)[1] for s in matches]
 
-    def _read_pnm_header(self, data: bytes) -> None:
+    def _read_pnm_header(self, data: bytes, /) -> None:
         """Read PNM header and initialize instance."""
         bpm = data[1:2] in b'14'
         match = re.search(
@@ -387,7 +423,7 @@ class NetpbmFile:
         self.depth = 3 if self.magicnum in b'P3P6P7 332' else 1
         self.tupltypes = [NetpbmFile.MAGIC_NUMBER[self.magicnum]]
 
-    def _read_pf_header(self, data: bytes) -> None:
+    def _read_pf_header(self, data: bytes, /) -> None:
         """Read PF header and initialize instance."""
         # there are no comments in these files
         match = re.search(
@@ -416,7 +452,7 @@ class NetpbmFile:
         else:
             raise ValueError(f'invalid magic number {self.magicnum!r}')
 
-    def _read_pg_header(self, data: bytes) -> None:
+    def _read_pg_header(self, data: bytes, /) -> None:
         """Read PG header and initialize instance."""
         match = re.search(
             br'(^(PG)[ ]+'
@@ -448,7 +484,7 @@ class NetpbmFile:
             raise ValueError(f'bitdepth {bitdepth} out of range')
 
     def _read_data(
-        self, fh: BinaryIO, byteorder: ByteOrder | None = None
+        self, fh: BinaryIO, /, *, byteorder: ByteOrder | None = None
     ) -> numpy.ndarray:
         """Return image data from open file as numpy array."""
         fh.seek(len(self.header))
@@ -488,16 +524,16 @@ class NetpbmFile:
             data = numpy.take(rgb332, data, axis=0)
         return data
 
-    def _tofile(self, fh: BinaryIO, pam: bool = False) -> None:
+    def _tofile(self, fh: BinaryIO, /, *, pam: bool = False) -> None:
         """Write Netpbm file."""
         fh.seek(0)
-        fh.write(self._header(pam))
+        fh.write(self._header(pam=pam))
         data = self.asarray(copy=False)
         if self.maxval == 1:
             data = numpy.packbits(data, axis=-1)
         data.tofile(fh)
 
-    def _header(self, pam: bool = False) -> bytes:
+    def _header(self, *, pam: bool = False) -> bytes:
         """Return file header as byte string."""
         if self.magicnum in b'PfPF4':
             raise ValueError('writing PF, Pf, and PF are not supported')
@@ -548,7 +584,6 @@ class NetpbmFile:
         )
 
     def __str__(self) -> str:
-        """Return information about Netpbm file."""
         return indent(
             repr(self),
             f'magicnumber: {self.magicnum.decode()}',
@@ -570,7 +605,7 @@ def indent(*args) -> str:
     )[2:]
 
 
-def main(argv: list[str] | None = None, test: bool = False) -> int:
+def main(argv: list[str] | None = None, /, *, test: bool = False) -> int:
     """Command line usage main function.
 
     Show images specified on command line or all images in  directory.
