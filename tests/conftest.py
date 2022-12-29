168e4db91a86b797dbf9d795d04bf72578247d9f
Christoph Gohlke <cgohlke@cgohlke.com>
2022-12-29 13:08:30 -0800

    Add tests/conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,25 @@
+# netpbmfile/tests/conftest.py
+
+import os
+import sys
+
+if os.environ.get('VSCODE_CWD'):
+    # work around pytest not using PYTHONPATH in VSCode
+    sys.path.insert(
+        0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+    )
+
+
+def pytest_report_header(config):
+
+    try:
+        pyversion = f'Python {sys.version.splitlines()[0]}'
+        import netpbmfile
+
+        return '{}\npackagedir: {}\nversion: netpbmfile {}'.format(
+            pyversion,
+            netpbmfile.__path__[0],
+            netpbmfile.__version__,
+        )
+    except Exception as exc:
+        return f'pytest_report_header failed: {exc!s}'
